417258d3c33d92e4bfbb77e98f48a56747258d12
Max Gee <maxgee@users.noreply.github.com>
2022-02-21 12:33:03 -0600

    Commit

--- server/index.js
+++ server/index.js
@@ -57,6 +57,7 @@ app.get('/api/get/pictures/:id', (req,res)=>{
         res.send(result);
     });
 })
+//Get all customers and vehicles
 app.get('/api/get/customers', (req,res)=>{
     const sqlGetAllCustomers = "SELECT * FROM customer";
     db.query(sqlGetAllCustomers,(err,result)=>{
@@ -66,6 +67,46 @@ app.get('/api/get/customers', (req,res)=>{
 app.get('/api/get/vehicles', (req,res)=>{
     const sqlGetAllVehicles = "SELECT * FROM vehicle";
     db.query(sqlGetAllVehicles,(err,result)=>{
+        res.send(result);
+    });
+})
+//Get customer and vehicle ID from Invoice ID
+app.get('/api/get/invoice/:id', (req,res)=>{
+    const invoiceID = req.params.id;
+    const getInvoice = "SELECT * FROM invoice WHERE invoiceID = ?";
+    db.query(getInvoice,[invoiceID],(err,result)=>{
+        res.send(result);
+    });
+})
+//Get customers and vehicles by ID
+app.get('/api/get/customer/:id', (req,res)=>{
+    const customerID = req.params.id;
+    const sqlGetCustomer = "SELECT * FROM customer WHERE customerID = ?";
+    db.query(sqlGetCustomer,[customerID],(err,result)=>{
+        res.send(result);
+    });
+})
+//Gets vehicle info given vehicleID
+app.get('/api/get/vehicle/:id', (req,res)=>{
+    const vehicleID = req.params.id; 
+    const sqlGetVehicle = "SELECT * FROM vehicle WHERE vehicleID = ?";
+    db.query(sqlGetVehicle,[vehicleID],(err,result)=>{
+        res.send(result);
+    });
+})
+//Get list of repairs givine InvoiceID
+app.get('/api/get/repairs/:id', (req,res)=>{
+    const invoiceID = req.params.id; 
+    const getRepairs = "SELECT * FROM invoiceLineItem WHERE invoiceID = ?";
+    db.query(getRepairs,[invoiceID],(err,result)=>{
+        res.send(result);
+    });
+})
+//Get list of parts given Invice ID
+app.get('/api/get/parts/:id', (req,res)=>{
+    const invoiceID = req.params.id; 
+    const getParts = "SELECT * FROM repairParts WHERE invoiceID = ?";
+    db.query(getParts,[invoiceID],(err,result)=>{
         res.send(result);
     });
 })
@@ -132,6 +173,48 @@ app.post('/api/insert/newVehicle', (req,res)=>{
         res.send(result);
     })
 });
+app.post('/api/insert/newInvoice', (req,res)=>{
+
+    const customerID = req.body.customerID
+    const vehicleID = req.body.vehicleID
+
+    const sqlCreateInvoice = "INSERT INTO invoice (customerID, vehicleID) VALUES (?,?)"
+    db.query(sqlCreateInvoice, [customerID, vehicleID], (err,result)=>{
+        console.log(err);
+        res.send(result);
+    })
+    
+});
+//Add Vehicle Repairs to the database
+app.post('/api/insert/newRepair', (req,res)=>{
+
+    const invoiceID = req.body.invoiceID
+    const itemName = req.body.itemName
+    const hours = req.body.hours
+    const laborRate = req.body.laborRate
+
+    const sqlAddRepair = "INSERT INTO invoiceLineItem (invoiceID, itemName, hours, laborRate) VALUES (?,?,?,?)"
+    db.query(sqlAddRepair, [invoiceID, itemName, hours, laborRate], (err,result)=>{
+        console.log(err);
+        res.send(result);
+    })
+    
+});
+//Add Reapair Parts to the DB
+app.post('/api/insert/addParts', (req,res)=>{
+
+    const invoiceID = req.body.invoiceID
+    const repairLineItemID = req.body.repairLineItemID
+    const partName = req.body.partName
+    const partPrice = req.body.partPrice
+
+    const sqlAddRepair = "INSERT INTO repairParts (invoiceID,repairLineItemID, partName, partPrice) VALUES (?,?,?,?)"
+    db.query(sqlAddRepair, [invoiceID,repairLineItemID, partName, partPrice], (err,result)=>{
+        console.log(err);
+        res.send(result);
+    })
+    
+});
 //
 //DELETE REQUEST
 //
@@ -190,6 +273,19 @@ app.put('/api/update/sale',(req,res)=>{
     const listingID = req.body.listingID
     const sqlUpdateSale = "UPDATE `seventy-1auto`.listings SET status = 'Sold' , salePrice = ?, saleDate = ? WHERE listingID = ?";
     db.query(sqlUpdateSale, [salePrice, saleDate, listingID],(err, result)=>{
+        res.send(result);
+        if (err) console.log(err)
+    })
+})
+//Update Invoice Information with Miles,Date,Recommendations
+app.put('/api/update/invoice',(req,res)=>{
+    const invoiceNumber = req.body.invoiceNumber
+    const currentMiles = req.body.currentMiles
+    const taxRate = req.body.taxRate
+    const invoiceDate = req.body.invoiceDate
+    const vehicleRecommendations = req.body.vehicleRecommendations
+    const updateInvoice = "UPDATE `seventy-1auto`.invoice SET vehicleMileage = ?, taxRate = ?, date = ? , recommendations = ? WHERE invoiceID = ?";
+    db.query(updateInvoice, [currentMiles, taxRate, invoiceDate, vehicleRecommendations,invoiceNumber],(err, result)=>{
         res.send(result);
         if (err) console.log(err)
     })
